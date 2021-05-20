a2ea63b5db4c14dfe9e4304f71809efc0a915c82
Aaron Miller <amiller@watheia.org>
2021-05-20 23:44:50 +0000

    add vercel-build

---
+++ vercel-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+npm install --global yarn @teambit/bvm && \
+    export PATH=$PATH:$HOME/bin && \
+    bvm install --force && \
+    bit config set analytics_reporting false && \
+    bit config set error_reporting false && \
+    bit config set no_warnings true && \
+    bit init --harmony && \
+    bit install && \
+    yarn next build && \
+    yarn next export
